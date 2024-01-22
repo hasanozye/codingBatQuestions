ce2416944f0377ca37be70f385daba8fd5a122f5
Hasan Özyer <hasanozyer06@gmail.com>
2024-01-22 14:20:31 +0300

    Initialized SameStarChar class for codingbat question. will solve it later.

---
+++ src/string2/SameStarChar.java
@@ -0,0 +1,23 @@
+package string2;
+
+public class SameStarChar {
+    public static void main(String[] args) {
+
+    }
+
+
+    public static boolean sameStarChar(String str){
+
+        int len = str.length();
+
+        for (int i = 0; i < len; i++) {
+
+            int firstStarIndex = str.indexOf("*");
+            int lastStarIndex = str.lastIndexOf("*");
+
+        }
+//TODO: UNDERSTAND THE PROBLEM.
+        return false;
+    }
+
+}
