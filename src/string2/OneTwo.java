7bd11e2311c2439800ee17b49d6f4b2a1918c68a
@hasan.ozyer <hasanozyer06@gmail.com>
2024-01-25 10:01:48 +0300

    solved the problem in hardway. But i elarned from it

--- src/string2/OneTwo.java
+++ src/string2/OneTwo.java
@@ -6,25 +6,27 @@ public class OneTwo {
         System.out.println(oneTwo("abc"));// → "bca"
         System.out.println(oneTwo("tca"));// → "cat"
         System.out.println(oneTwo("tcagdo"));// → "catdog"
+        System.out.println(oneTwo("abcdefx"));// → "bcaefd"
     }
 
     public static String oneTwo(String str) {
 
         int len = str.length();
         String result = "";
-        char ch = 1;
-
-
-        for (int i = 0; i <= len - 3; i += 3) {
-
-
-            result += str.substring(i + 1, i + 3);
-            ch = str.charAt(i);
-
+        if (len < 3) {
+            return result;
         }
 
-        return result + ch;
-        //TODO: solve this problem for edge cases
+        int lastChars = len % 3;
+
+
+        for (int i = 0; i < len - lastChars; i += 3) {
+
+            String firstCh = str.substring(i, i + 1);
+            String otherChs = str.substring(i + 1, i + 3);
+            result += otherChs + firstCh;
+        }
+        return result;
 
     }
 }
