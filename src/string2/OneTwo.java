cb5d29f0a4b30c8da0c16db260ae5d106ed92d09
@hasan.ozyer <hasanozyer06@gmail.com>
2024-01-25 10:07:06 +0300

    considered alternative approach by chatgpt. I feel liek dumb lol

--- src/string2/OneTwo.java
+++ src/string2/OneTwo.java
@@ -28,5 +28,16 @@ public class OneTwo {
         }
         return result;
 
+ /*       String result = "";
+
+        for (int i = 0; i < str.length() - 2; i += 3) {
+            result += str.charAt(i + 1) + "" + str.charAt(i + 2) + str.charAt(i);
+        }
+
+        return result;
+
+  */
+
+
     }
 }
