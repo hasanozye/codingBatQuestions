c05c702d6afb053c2c3f3aa8cda1c55ef34e7a8a
hasan.ozyer <hasanozyer06@gmail.com>
2024-01-30 13:19:42 +0300

    Initialized BigDiff problem in codingBat. Will sovle it later today.

---
+++ src/array2/BigDiff.java
@@ -0,0 +1,18 @@
+package array2;
+
+public class BigDiff {
+
+    public static void main(String[] args) {
+
+
+        System.out.println(bigDiff(new int[]{10, 3, 5, 6})); //→ 7
+        System.out.println(bigDiff(new int[]{7, 2, 10, 9})); //→ 8
+        System.out.println(bigDiff(new int[]{2, 10, 7, 2})); //→ 8
+    }
+
+    public static int bigDiff(int[] arr){
+
+
+
+    }
+}
