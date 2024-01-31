9875df7b0238fb66d4c16c1de2aafd1b4f0fcd3d
hasan.ozyer <hasanozyer06@gmail.com>
2024-01-31 22:24:05 +0300

    Solved BigDiff problem in codingBat.

--- src/array2/BigDiff.java
+++ src/array2/BigDiff.java
@@ -10,8 +10,18 @@ public class BigDiff {
         System.out.println(bigDiff(new int[]{2, 10, 7, 2})); //→ 8
     }
 
-    public static int bigDiff(int[] arr){
+    public static int bigDiff(int[] nums){
 
+        int max = nums[0];
+        int min = nums[0];
+
+        for (int num : nums){
+            min = Math.min(min,num);
+            max = Math.max(max,num);
+
+        }
+
+        return max - min;
 
 
     }
