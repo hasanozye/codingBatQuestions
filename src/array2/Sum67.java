3116b9ddd1cdc110aafcd37bac9294a9e1e6ab05
hasan.ozyer <hasanozyer06@gmail.com>
2024-02-02 12:02:51 +0300

    Initialized Sum67 class. Will solve it today in the evening.

---
+++ src/array2/Sum67.java
@@ -0,0 +1,20 @@
+package array2;
+
+public class Sum67 {
+    public static void main(String[] args) {
+        System.out.println(sum67(new int[]{1, 2, 2}));// → 5
+        System.out.println(sum67(new int[]{1, 2, 2, 6, 99, 99, 7}));// → 5
+        System.out.println(sum67(new int[]{1, 1, 6, 7, 2}));// → 4
+        System.out.println(sum67(new int[]{6, 8, 1, 6, 7}));// → 0
+        System.out.println(sum67(new int[]{6, 7, 1, 6, 7, 7}));// → 8
+    }
+
+
+    public static int sum67(int[] nums) {
+
+        int fin = 0 ;
+        return fin;
+
+    }
+
+}
