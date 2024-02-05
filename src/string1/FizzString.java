b440758944369b18f3daedce16b9241cd83d4f9d
hasan.ozyer <hasanozyer06@gmail.com>
2024-02-05 11:33:35 +0300

    Solved FizzString problem on codingBat

--- src/string1/FizzString.java
+++ src/string1/FizzString.java
@@ -3,13 +3,34 @@ package string1;
 public class FizzString {
 
     public static void main(String[] args) {
-
+        System.out.println(fizzString("fig")); //→ "Fizz"
+        System.out.println(fizzString("dib")); //→ "Buzz"
+        System.out.println(fizzString("fib")); //→ "FizzBuzz"
     }
 
 
     public static String fizzString(String str) {
 
-        for (int i = 0; i < str.length(); i++) {
+        boolean startsWith = str.startsWith("f");
+        boolean endsWith = str.endsWith("b");
+
+        if(startsWith && endsWith){
+
+            return "FizzBuzz";
+
+        }else if(startsWith){
+
+            return "Fizz";
+
+        }else if(endsWith){
+
+            return "Buzz";
+        }else{
+
+            return str;
+        }
+
+        /*for (int i = 0; i < str.length(); i++) {
 
             if (str.charAt(0) == 'f' && str.charAt(str.length() - 1) == 'b') {
 
@@ -24,7 +45,7 @@ public class FizzString {
 
         }
 
-        return "";
+        return "";*/
 
     }
 }
