3747b3a38c4530f02c022faf31fc8f0497f4abaa
hasan.ozyer <hasanozyer06@gmail.com>
2024-02-05 11:41:27 +0300

    Solved FizzArray problem on codingBat

--- src/array2/FizzArray.java
+++ src/array2/FizzArray.java
@@ -1,16 +1,27 @@
 package array2;
 
+import java.util.Arrays;
+
 public class FizzArray {
 
     public static void main(String[] args) {
-
+        System.out.println(Arrays.toString(fizzArray(4))); //→ [0, 1, 2, 3]
+        System.out.println(Arrays.toString(fizzArray(1))); //→ [0]
+        System.out.println(Arrays.toString(fizzArray(10))); //→ [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     }
 
 
     public static int[] fizzArray(int n) {
 
+        int[] arr = new int[n];
 
-        return new int[]{1, 2, 3};
+        for (int i = 0; i < n; i++) {
+
+            arr[i] += i;
+
+        }
+
+        return arr;
 
     }
 }
