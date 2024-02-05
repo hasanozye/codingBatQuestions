646e3a702ff5796935c1f436d1eb3f899ba304e6
hasan.ozyer <hasanozyer06@gmail.com>
2024-02-05 10:35:57 +0300

    Initialized FizzArray problem on codingBat. Will solve it today later.

---
+++ src/array2/FizzArray.java
@@ -0,0 +1,16 @@
+package array2;
+
+public class FizzArray {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public static int[] fizzArray(int n) {
+
+
+        return new int[]{1, 2, 3};
+
+    }
+}
