405095dbd3c06c54350b11d50096f8ad6ecd4726
hasan.ozyer <hasanozyer06@gmail.com>
2024-02-07 11:42:15 +0300

    In O notation space time compelxity i m pretty much screwed up but i will find another solution lul.

---
+++ src/array2/No14.java
@@ -0,0 +1,48 @@
+package array2;
+
+public class No14 {
+
+    public static void main(String[] args) {
+
+        System.out.println(no14(new int[]{2, 3, 4, 1})); // false
+        System.out.println(no14(new int[]{1, 2, 3})); //  true
+        System.out.println(no14(new int[]{1, 2, 3, 4})); // false
+        System.out.println(no14(new int[]{2, 3, 4})); // true
+
+    }
+
+    public static boolean no14(int[] nums) {
+
+        boolean is1or4 = false;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] == 1) {
+
+                for (int j = i; j < nums.length; j++) {
+
+                    if (nums[j] == 4) {
+                        return false;
+
+                    }
+
+                }
+
+
+            } else if (nums[i] == 4) {
+
+                for (int j = i; j < nums.length; j++) {
+
+                    if (nums[j] == 1) {
+                        return false;
+
+                    }
+
+                }
+            }
+
+        }
+        return true;
+
+    }
+}
