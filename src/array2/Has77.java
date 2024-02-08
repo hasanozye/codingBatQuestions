18b7e95ed0dafff60d5fa7a70b4bb86759d9c82e
hasan.ozyer <hasanozyer06@gmail.com>
2024-02-08 11:30:17 +0300

    Solved Has77 problem in Java

--- src/array2/Has77.java
+++ src/array2/Has77.java
@@ -1,4 +1,35 @@
 package array2;
 
 public class Has77 {
+
+    public static void main(String[] args) {
+
+        System.out.println(has77(new int[]{2, 7, 2, 2, 7, 2})); // false
+        System.out.println(has77(new int[]{1, 7, 7})); // true
+        System.out.println(has77(new int[]{1, 7, 1, 7})); // true
+        System.out.println(has77(new int[]{1, 7, 1, 1, 7})); // false
+
+
+    }
+
+
+    public static boolean has77(int[] nums) {
+
+        for (int i = 0; i < nums.length - 1; i++) {
+
+            if ((nums[i] == 7 && nums[i + 1] == 7)) {
+
+                return true;
+
+            } else if (nums.length - i != 2 && nums[i] == 7 && nums[i + 2] == 7) {
+
+                return true;
+
+            }
+
+        }
+
+        return false;
+
+    }
 }
