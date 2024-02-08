6e386ed136485019116efcbe4b0ebd0a8766e806
hasan.ozyer <hasanozyer06@gmail.com>
2024-02-08 11:18:49 +0300

    added diff parameter to make things better

--- src/array2/MatchUp.java
+++ src/array2/MatchUp.java
@@ -13,19 +13,16 @@ public class MatchUp {
 
 
     public static int matchUp(int[] nums1, int[] nums2) {
-
         int count = 0;
 
         for (int i = 0; i < nums1.length; i++) {
-
-            if (Math.abs(nums1[i] - nums2[i]) <= 2 && Math.abs(nums1[i] - nums2[i]) != 0) {
-
+            int diff = Math.abs(nums1[i] - nums2[i]);
+            if (diff <= 2 && diff > 0) {
                 count++;
-
             }
-
         }
 
         return count;
     }
+
 }
