f35b89c90185508b24aa7ade0440cfb63a97b30d
hasan.ozyer <hasanozyer06@gmail.com>
2024-02-10 00:13:37 +0300

    Initialized SameEnds class. Will solve it later.

---
+++ src/array2/SameEnds.java
@@ -0,0 +1,21 @@
+package array2;
+
+public class SameEnds {
+
+    public static void main(String[] args) {
+
+        System.out.println(sameEnds(new int[]{5, 6, 45, 99, 13, 5, 6}, 1)); // false
+        System.out.println(sameEnds(new int[]{5, 6, 45, 99, 13, 5, 6}, 2)); // true
+        System.out.println(sameEnds(new int[]{5, 6, 45, 99, 13, 5, 6}, 3)); // false
+
+    }
+
+
+    public static boolean sameEnds(int[] nums, int len) {
+
+        boolean flag = false;
+
+        return false;
+
+    }
+}
