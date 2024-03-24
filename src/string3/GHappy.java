bd09af604fceb5bb32514b704a80de5f03c5aa2f
hasan.ozyer <hasanozyer06@gmail.com>
2024-03-24 14:40:05 +0300

    Initialized GHappy problem on codingbat

---
+++ src/string3/GHappy.java
@@ -0,0 +1,22 @@
+package string3;
+
+public class GHappy {
+
+    public static void main(String args[]){
+
+        System.out.println(gHappy("xxggxx")); // -> True
+        System.out.println(gHappy("xxgxx")); // -> False
+        System.out.println(gHappy("xxggyygxx")); // -> False
+
+    }
+
+
+    public static boolean gHappy(String str){
+
+
+
+
+    }
+
+
+}
