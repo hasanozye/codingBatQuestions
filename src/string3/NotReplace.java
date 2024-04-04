666e322cdf6bfae9e111450e75df48529396a3d8
hasan.ozyer <hasanozyer06@gmail.com>
2024-04-04 09:17:37 +0300

    Not Replace is initalized

---
+++ src/string3/NotReplace.java
@@ -0,0 +1,22 @@
+package string3;
+
+public class NotReplace {
+
+    public static void main(String[] args) {
+
+        System.out.println(notReplace("is test")); // is not test
+        System.out.println(notReplace("is-is")); // is not-is not
+        System.out.println(notReplace("This is right")); // This is not right
+
+
+    }
+
+
+
+    public static String notReplace(String str){
+
+
+
+
+    }
+}
