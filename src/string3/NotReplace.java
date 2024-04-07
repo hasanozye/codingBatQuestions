7809501ef65c30f5bcece45277fdbdc3487663e7
hasan.ozyer <hasanozyer06@gmail.com>
2024-04-07 23:37:39 +0300

    solvew with the help of regex

--- src/string3/NotReplace.java
+++ src/string3/NotReplace.java
@@ -2,21 +2,16 @@ package string3;
 
 public class NotReplace {
 
-    public static void main(String[] args) {
-
-        System.out.println(notReplace("is test")); // is not test
-        System.out.println(notReplace("is-is")); // is not-is not
-        System.out.println(notReplace("This is right")); // This is not right
-
-
+    public static String notReplace(String str) {
+        // Use regular expression with word boundary (\b) to find standalone "is"
+        // and replace with "is not"
+        return str.replaceAll("\\bis\\b", "is not");
     }
 
-
-
-    public static String notReplace(String str){
-
-
-
-
+    public static void main(String[] args) {
+        // Test the method with given examples
+        System.out.println(notReplace("is test")); // should print "is not test"
+        System.out.println(notReplace("is-is")); // should print "is not-is not"
+        System.out.println(notReplace("This is right")); // should print "This is not right"
     }
 }
